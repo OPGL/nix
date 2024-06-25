a49086cfc9c4096da598445ed6acd18e627c306f
opgl <opgl@duck.com>
2024-06-25 09:46:51 +0200

    initial commit

---
+++ hosts/default/configuration.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nixos";
+
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Warsaw";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "pl";
+    variant = "";
+  };
+
+  console.keyMap = "pl2";
+
+  users.users.opgl = {
+    isNormalUser = true;
+    description = "Dawid";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+     "opgl" = import ./home.nix;
+    };   
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  programs.direnv.enable = true;
+  environment.systemPackages = with pkgs; [
+  
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  system.stateVersion = "24.05";
+
+}
